318b7d00d0bb865081e64031497e22ccde88a73f
P.S.V.R <pmq2001@gmail.com>
2012-02-29 11:01:07 +0800

    new rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+T2kuPragmatic::Application.config.secret_token = '505b4071cc698de5284f7d5145535a48c8e3be1f38d38b6d018eb5427668efa20475f487efca7ba18950b3470f3141b92d842338b6cd118911d58bbd7d560c9e'
